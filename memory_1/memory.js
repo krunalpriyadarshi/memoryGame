a27a0e0c757874705b80e0c8061e488fe3ee1ee7
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2023-08-15 09:42:13 -0400

    issue: highscore resets

--- memory_1/memory.js
+++ memory_1/memory.js
@@ -190,9 +190,13 @@ $(function () {
                                     if (highScore> parseInt(sessionStorage.getItem("memoryGameHighScore"), 10)){
                                         sessionStorage.setItem("memoryGameHighScore", highScore);
                                         console.log("new highscore: ", highScore);
-                                        console.log("new highscore: ", parseInt(sessionStorage.getItem("memoryGameHighScore"), 10));     
-                                    }                               
-                                    alert("Congratulations! You've matched all pairs. \nYour high score is: " + highScore);
+                                        console.log("new highscore from session: ", parseInt(sessionStorage.getItem("memoryGameHighScore"), 10)); 
+                                        highScoreElement = document.getElementById("high_score");} 
+                                        highScoreElement.textContent = "HighScore: " + parseInt(sessionStorage.getItem("memoryGameHighScore"), 10);                              
+                                        alert("Congratulations! You've matched all pairs. \nYour high score is: " + highScore);       
+                                        //alert("Congratulations! You've matched all pairs.\nYour high score is: " + highScore + "\n\nClick OK to go back to the homepage.");
+                                        // Redirect to index.html when OK is clicked
+                                        //window.location.href = "index.html";
                                 }
 
                             } else {
